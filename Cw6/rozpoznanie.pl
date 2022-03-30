fedadaa65f721636d875ed4f08846af0b38b5b51
PDondalski <73539016+PDondalski@users.noreply.github.com>
2022-03-30 15:45:43 +0200

    Add files via upload

---
+++ Cw6/rozpoznanie.pl
@@ -0,0 +1,15 @@
+%warunki zakoñczenia rekurencji
+wielomian(X,X).
+wielomian(C,_):-number(C).
+
+%rekurencja
+wielomian(-W,X):-wielomian(W,X).
+wielomian(W1+W2,X):-wielomian(W1,X),
+    wielomian(W2,X).
+wielomian(W1-W2,X):-wielomian(W1,X),
+    wielomian(W2,X).
+wielomian(W1*W2,X):-wielomian(W1,X),
+    wielomian(W2,X).
+wielomian(W^N,X):-integer(N),
+    N>1,
+    wielomian(W,X).
